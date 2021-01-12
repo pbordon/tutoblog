ef90bb639e0b1d5de9628847a931e051c5bce9fb
pbordon <pbordon@gmail.com>
2021-01-12 11:10:42 -0300

    Segundo comit

--- config/database.php
+++ config/database.php
@@ -82,7 +82,7 @@ return [
             'driver' => 'sqlsrv',
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '4444'),
+            'port' => env('DB_PORT', '3717'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
